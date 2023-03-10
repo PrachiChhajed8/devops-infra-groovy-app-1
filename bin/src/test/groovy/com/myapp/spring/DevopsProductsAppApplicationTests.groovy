83f99e6fff30a5f3aa7fffc7e17fd6d42582c334
PrachiChhajed8 <prachichhajed@gmail.com>
2023-03-10 15:23:33 +0530

    Jenkins file git url changed

---
+++ bin/src/test/groovy/com/myapp/spring/DevopsProductsAppApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.myapp.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DevopsProductsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+//Changes done by Prachi
